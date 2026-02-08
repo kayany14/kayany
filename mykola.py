de635e4ff789af0c5d99d780be99df670b4df6ed
kayany14 <danteleno71@gmail.com>
2026-02-08 17:31:19 +0200

    this file for mykola kyn

---
+++ mykola.py
@@ -0,0 +1,10 @@
+from customtkinter import *
+from tkinter import *
+window = CTk()
+btn = CTkButton(window, text="Click Me")
+btn.pack()
+window.configure(fg_color="yellow")
+window.title("микола кун")
+window.geometry("400x300")
+window.mainloop()
+
